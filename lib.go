5a89af8792b7852054bac0ab0aa72b6df013700e
Abiria <abiria.dev@gmail.com>
2024-01-24 16:51:21 +0900

    feat: implement `nextInt` method

--- lib.go
+++ lib.go
@@ -14,14 +14,30 @@ func newRandom(seed int64) Random {
 	}
 }
 
-func (r Random) next(bits int32) int32 {
+func (r *Random) next(bits int32) int32 {
 	r.seed = (r.seed*0x5DEECE66D + 0xB) & mask
 
 	return int32(r.seed >> (48 - bits))
 }
 
-func main() {
-	r := newRandom(1000)
+func (r *Random) nextInt(bound int32) int32 {
+	var bits, val int32
 
-	fmt.Println(r.next(32))
+	for ok := true; ok; ok = bits-val+(bound-1) < 0 {
+		bits = r.next(31)
+		val = bits % bound
+	}
+
+	return val
+}
+
+func main() {
+	r := newRandom(1)
+
+	// fmt.Println(r.next(32))
+	fmt.Println(r.nextInt(10))
+	fmt.Println(r.nextInt(10))
+	fmt.Println(r.nextInt(10))
+	fmt.Println(r.nextInt(10))
+	fmt.Println(r.nextInt(10))
 }
